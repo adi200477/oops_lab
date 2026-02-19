53dc34965568716df9f2a2d4188c512c7c1a8bea
Adithyan2004 <130374048+adi200477@users.noreply.github.com>
2026-02-19 11:18:46 +0530

    Add files via upload

---
+++ score.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.*;
+
+class student
+{
+ String name = "Tom";
+ String batch = "MCA";
+}
+
+class sports extends student
+{
+ String sports = "Cricket";
+}
+
+class result extends sports
+{
+ String result = "Win";
+ void display()
+ {
+  System.out.println("Student name :"+name);
+  System.out.println("Student class :"+batch);
+  System.out.println("Student participated in :"+sports);
+  System.out.println("Sports result :"+result);
+ }
+}
+
+class score
+{
+ public static void main(String args[])
+ {
+  result r = new result();
+  r.display();
+ }
+}
